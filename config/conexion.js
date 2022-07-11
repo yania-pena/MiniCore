d1893dc26fac25ea4caeec5b54ea7f6120c66643
yania-pena <yania.pena@udla.edu.ec>
2022-07-11 01:51:37 -0500

    Update conexion.js

--- config/conexion.js
+++ config/conexion.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql')
 
 const connection = mysql.createConnection({
-    host: '	lcpbq9az4jklobvq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: '	l4o28izwn0mlwumh',
+    host: 'lcpbq9az4jklobvq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'l4o28izwn0mlwumh',
     password: 'u45x5wvdbauod19s',
     database: 'i14rfxqyqln3e88c'
 
